f249509ee8bccbc38fc0e9cf3d4979e83022d7c3
bakoalex <alex.bako@europ-assistance.hu>
2017-07-23 12:18:50 +0200

    Testing on mobile

--- js/enviz.js
+++ js/enviz.js
@@ -6,3 +6,13 @@ $(".galery-item-overlay").hover(
     $(this).removeClass('.galery-item-description');
   }
 )
+
+var parallaxItemHeight = $('.parallax').outerHeight();
+function parallax() {
+  var scrolled = $(window).scrollTop();
+  $('parallax-bg').css('height', (parallaxItemHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+  parallax();
+});
